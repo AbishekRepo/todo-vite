266585cf6fc712d12e41c5fb4211a1067fdb3ec7
Abishek <abishekhttps@gmail.com>
2023-11-10 23:15:43 +0530

    ratings app included

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -1,19 +1,24 @@
 import HomePage, { AppList } from "../HomePage";
 import TodoApp from "../TodoApp";
+import Ratings from "../Ratings/Ratings";
 
 export default [
-    {
-        path: '/',
-        component: HomePage,
-        children: [
-            {
-                path: '',
-                component: AppList
-            },
-            {
-                path: 'todo-app',
-                component: TodoApp
-            },
-        ]
-    }
-]
+  {
+    path: "/",
+    component: HomePage,
+    children: [
+      {
+        path: "",
+        component: AppList,
+      },
+      {
+        path: "todo-app",
+        component: TodoApp,
+      },
+      {
+        path: "ratings-app",
+        component: Ratings,
+      },
+    ],
+  },
+];
--- src/TodoApp/index.jsx
+++ src/TodoApp/index.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import List from '../components/List';
-import Form from '../components/Form';
-import { useTodoAppContext, TodoAppProvider } from './todoContext/TodoContext';
+import React from "react";
+import List from "./components/List";
+import Form from "./components/Form";
+import { useTodoAppContext, TodoAppProvider } from "./todoContext/TodoContext";
 
 const Todo = () => {
-    const { form, setForm, list, handleSubmit, close } = useTodoAppContext();
+  const { form, setForm, list, handleSubmit, close } = useTodoAppContext();
 
-    return (
-        <div>
-            <div className="container">
-                <Form setForm={setForm} form={form} handleSubmit={handleSubmit} />
-            </div>
-            <div className="list-elements">
-                {list.map((item, index) => {
-                    return <List key={index} list={item} close={close} />;
-                })}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="container">
+        <Form setForm={setForm} form={form} handleSubmit={handleSubmit} />
+      </div>
+      <div className="list-elements">
+        {list.map((item, index) => {
+          return <List key={index} list={item} close={close} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 const TodoApp = () => {
-    return (
-        <TodoAppProvider>
-            <Todo />
-        </TodoAppProvider>
-    )
-}
+  return (
+    <TodoAppProvider>
+      <Todo />
+    </TodoAppProvider>
+  );
+};
 
-export default TodoApp
+export default TodoApp;
